f65188d800e924f0a35696a6d0795e6d0a6a1959
kain <kainpro@foxmail.com>
2025-12-31 23:21:44 +0800

    refactor: remove jobs component and associated files from the application; update routing to reflect changes

---
+++ src/app/analytics/analytics.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { SharedModule } from '@shared';
+
+import { Layout } from '../__layout/layout';
+
+@Component({
+  standalone: true,
+  imports: [SharedModule, Layout],
+  selector: 'app-analytics',
+  templateUrl: './analytics.html'
+})
+export class Analytics {}
+
