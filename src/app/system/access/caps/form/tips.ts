9b766110412dd163107a86cc2620d993ac642b35
kain <kainpro@foxmail.com>
2026-02-21 19:13:52 +0800

    feat: 更新表单和表格字段名称，统一为“标识”和“描述”

--- src/app/system/access/caps/form/tips.ts
+++ src/app/system/access/caps/form/tips.ts
@@ -1,12 +1,12 @@
 export const tips = {
   code: {
     default: {
-      required: '能力代码不能为空'
+      required: '能力标识不能为空'
     }
   },
   description: {
     default: {
-      required: '能力描述不能为空'
+      required: '能力标识不能为空'
     }
   }
 };
