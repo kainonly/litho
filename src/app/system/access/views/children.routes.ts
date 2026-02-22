2ceb8605ce0ce2f50a7b16838b3f79045056401e
kain <kainpro@foxmail.com>
2026-02-22 12:03:02 +0800

    feat: 删除首页组件，更新路由配置以重定向到系统页面

--- src/app/system/access/views/children.routes.ts
+++ src/app/system/access/views/children.routes.ts
@@ -2,12 +2,8 @@ import { Routes } from '@angular/router';
 
 export const childrenRoutes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home').then(m => m.Home)
-  },
-  {
     path: ':nav',
     loadComponent: () => import('./routes/routes').then(m => m.Routes)
   },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'system', pathMatch: 'full' }
 ];
--- src/app/system/access/views/views.ts
+++ src/app/system/access/views/views.ts
@@ -1,99 +1,32 @@
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { NzModalService } from 'ng-zorro-antd/modal';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { Global, SharedModule } from '@shared';
-import { Menu } from '@shared/models';
-
-import { Form, FormInput } from './form/form';
 
 @Component({
   imports: [SharedModule],
   selector: 'app-system-views',
-  templateUrl: './views.html',
+  template: `
+    <div class="litho-split-container">
+      <div class="litho-split-list-panel" style="width: 200px; overflow: hidden auto">
+        <ul style="border: none; height: 100%" nz-menu nzMode="inline">
+          @for (nav of global.navs; track nav.key) {
+            <li nz-menu-item nzMatchRouter>
+              <a [routerLink]="['/system', 'access', 'views', nav.link]">
+                <nz-icon [nzType]="nav.icon" />
+                {{ nav.name }}
+              </a>
+            </li>
+          }
+        </ul>
+      </div>
+
+      <div class="litho-split-detail-panel">
+        <router-outlet />
+      </div>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Views implements OnInit {
+export class Views {
   global = inject(Global);
-
-  private destroyRef = inject(DestroyRef);
-  private modal = inject(NzModalService);
-  private message = inject(NzMessageService);
-
-  // m = this.global.setModel(`menus`, this.menus, {
-  //   q: ''
-  // });
-
-  ngOnInit(): void {
-    // this.m.pagesize.set(100);
-    // this.m
-    //   .ready()
-    //   .pipe(takeUntilDestroyed(this.destroyRef))
-    //   .subscribe(() => {
-    //     this.getData();
-    //   });
-  }
-
-  getData(refresh = false): void {
-    // if (refresh) {
-    //   this.m.page.set(1);
-    // }
-    // let params = new HttpParams();
-    // const { q } = this.m.search;
-    // if (q) {
-    //   params = params.set('q', q);
-    // }
-    // this.m.fetch(params).pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
-  }
-
-  open(data?: Menu): void {
-    this.modal.create<Form, FormInput>({
-      nzTitle: !data ? '新增导航' : `修改导航【${data.name}】`,
-      nzContent: Form,
-      nzData: {
-        data
-      },
-      nzOnOk: () => {
-        this.getData(true);
-      }
-    });
-  }
-
-  delete(data: Menu): void {
-    // this.global.deleteConfirm(`导航【${data.name}】`, () => {
-    //   this.menus
-    //     .delete([data.id])
-    //     .pipe(takeUntilDestroyed(this.destroyRef))
-    //     .subscribe(() => {
-    //       this.message.success(`删除成功`);
-    //       this.m.clearSelection(data.id);
-    //       this.getData(true);
-    //     });
-    // });
-  }
-
-  bulkDelete(): void {
-    // this.global.bulkDeleteConfirm(() => {
-    //   this.menus
-    //     .delete([...this.m.selection().keys()])
-    //     .pipe(takeUntilDestroyed(this.destroyRef))
-    //     .subscribe(() => {
-    //       this.message.success(`删除成功`);
-    //       this.m.clearSelections();
-    //       this.getData(true);
-    //     });
-    // });
-  }
-
-  sort(event: CdkDragDrop<string[]>, values: Menu[]): void {
-    moveItemInArray(values, event.previousIndex, event.currentIndex);
-    // this.menus
-    //   .sort(values.map(v => v.id))
-    //   .pipe(takeUntilDestroyed(this.destroyRef))
-    //   .subscribe(() => {
-    //     this.message.success('排序成功');
-    //     this.getData(true);
-    //   });
-  }
 }
